5754efb9178a76605a4d330a18ca98bc74fb3f27
Mike Knoles <MKnoles78@gmail.com>
2021-11-28 19:17:39 -0500

    added pseudocode

--- src/Unique.java
+++ src/Unique.java
@@ -7,18 +7,43 @@ public class Unique {
 	// gets 5 unique numbers from the user
 	public void getNumbers() {
 		Scanner input = new Scanner(System.in);
-		
-		//Create an array of five elements
-		int count = 0; //number of uniques read
-		int entered = 0; //number of entered numbers
-		
+
+		// Create an array of five elements
+		int numbers[] = new int[5]; // stores list of inputs from user in array
+
+		int count = 0; // number of uniques read
+		int entered = 0; // number of entered numbers
+
 		while (entered < numbers.length) {
 			System.out.print("Enter number: ");
-			//TODO code goes here
-			//code to to retrieve inputs
-			//code to validate inputs using if statement
-			{
+			// code to to retrieve inputs
+			int number = input.nextInt();
+
+			// code to validate inputs using if statement
+			// inputs five numbers, each of which is between 10 and 100, inclusive
+			// write if statement that validates the input
+			if (10 <= number && number <= 100) {
+				// flags whether this number already exist
+				boolean containsNumber = false;
+
+				// increment number of entered numbers
+				entered++;
+
+				// compare the user input to the unique numbers in the array using a
+				// for statement. If number is unique , store new number
+
+				// add the user input to the array only if the number is not already in the
+				// array
+				if (!containsNumber) {
+					// write code to add the number to the array and increment unique items input
+
+				} else
+					System.out.printf("%d has already been entered\n", number);
+			} else
+				System.out.println("number must be between 10 and 100");
+
+			// print the list of unique values
+			// write code to output the contents of the array
 		}
 	}
-
 }
